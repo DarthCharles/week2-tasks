553838646d3370e271b661b81c7fa5e764bc06b8
Darthcharles <carlos.darth@gmail.com>
2015-08-12 17:45:19 -0700

    refactoring problem in GO

--- alien-language/alien-languaje.js
+++ alien-language/alien-languaje.js
@@ -3,3 +3,20 @@ var dictionary = ["abc",
 "dac",
 "dbc",
 "cba"];
+
+
+
+var str = "abc";
+
+for (var i = 0; i < dictionary.length; i++) {
+  var str = dictionary[i];
+  var patt1 =/[ab][bc][ca]/g
+  var matches = str.match(patt1);
+
+  console.log(matches);
+}
+
+// [ab][bc][ca]
+// abc
+// (abc)(abc)(abc)
+// (zyx)bc
--- minimum-scalar/minimum-scalar.go
+++ minimum-scalar/minimum-scalar.go
@@ -4,24 +4,52 @@ package main
 import "fmt"
 import "log"
 import "bufio"
+import "strings"
+import "strconv"
 import "os"
 
 func main() {
-    file, err := os.Open(os.Args[1])
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+  file, err := os.Open(os.Args[1])
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer file.Close()
 
   scanner := bufio.NewScanner(file)
   var lines []string
-    for scanner.Scan() {
-        //'scanner.Text()' represents the test case, do something with it
-        //fmt.Println(scanner.Text())
-      //  lines.append(lines, scanner.Text())
-        lines = append(lines, scanner.Text())
+  for scanner.Scan() {
+    //'scanner.Text()' represents the test case, do something with it
+    //fmt.Println(scanner.Text())
+    //  lines.append(lines, scanner.Text())
+    lines = append(lines, scanner.Text())
+  }
+
+
+  for i := 1; i < len(lines); i+=3 {
+    first_array_s := strings.Split(lines[i + 1], " ")
+    second_aray_s := strings.Split(lines[i + 2], " ")
+    var first_array_i, second_array_i []int
+
+    for i := 0; i < len(first_array_s); i++ {
+      k, _ := strconv.Atoi(first_array_s[i])
+      first_array_i = append(first_array_i, k)
+
+      k, _ = strconv.Atoi(second_aray_s[i])
+      second_array_i = append(second_array_i, k)
+    }
+    res :=
+
+    for (var j = 0; j < first_aray.length; j++) {
+      res += first_aray[j] * second_aray[j];
     }
 
-fmt.Println(lines[0])
+    for i := 0; i < len(first_array_i); i++ {
 
+    }
+    fmt.Println(first_array_i)
+    fmt.Println(second_array_i)
+
+  }
+
+  //fmt.Println(first_aray)
 }
